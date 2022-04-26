dee5f071b0e213e78a6e9f83b4cf5fd79f39af03
Sven Sonnborn <sonnborn.sven@web.de>
2022-04-26 13:25:33 +0200

    Changed Slide to extra Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,31 +5,5 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-  images = ['coding.jpg', 'learning.jpg', 'university.jpg'];
-  headlines = [
-    'Find out what you like doing best and make it your job',
-    'If you are willing to be a self-learner, you will develop yourself',
-    'Allways learning',
-  ];
-
-  currentImg = 0;
-  showImg = true;
-
-  ngOnInit() {
-    this.updateImg();
-  }
-
-  //Updates images every 8sec and reloads image everytime -> Animation is played everytime
-  updateImg() {
-    setInterval(() => {
-      this.currentImg++;
-      this.currentImg = this.currentImg % this.images.length;
-      this.showImg = false;
-
-      setTimeout(() => {
-        this.showImg = true;
-      }, 10);
-    }, 8000);
-  }
+export class AppComponent{
 }
