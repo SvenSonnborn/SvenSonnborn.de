0a73ba922560cfa014bf997e0128359b73b69738
Sven Sonnborn <sonnborn.sven@web.de>
2022-04-26 12:22:16 +0200

    Addes Img and started to work on slideshow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'svensonnborn.de';
+export class AppComponent implements OnInit {
+  images = ['university.jpg', 'learning.jpg', 'coding.jpg'];
+  currentImg = 0;
+  showImg = true;
+
+  ngOnInit() {
+    this.updateImg();
+  }
+
+  //Updates images every 8sec and reloads image everytime -> Animation is played everytime
+  updateImg() {
+    setInterval(() => {
+      this.currentImg++;
+      this.currentImg = this.currentImg % this.images.length;
+      this.showImg = false;
+
+      setTimeout(() => {
+        this.showImg = true;
+      }, 10);
+    }, 8000);
+  }
 }
